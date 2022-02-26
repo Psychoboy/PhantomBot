fa5cf48651eb97625102b4ba5beb74734bec6952
Psychoboy <psycoboy@gmail.com>
2022-02-26 14:14:04 -0700

    Create wheelSpin.js

---
+++ javascript-source/custom/wheelSpin.js
@@ -0,0 +1,39 @@
+(function() {
+    var cost = 10000,
+        userCoolDown = 3600, //1 hour in seconds
+        users = [];
+
+    $.bind('command', function(event) {
+        var sender = event.getSender().toLowerCase(),
+            command = event.getCommand(),
+            args = event.getArgs(),
+            action = args[0],
+            actionArg1 = args[1],
+            actionArg2 = args[2];
+
+        if(command.equalsIgnoreCase('wheelspin')) {
+            if ($.getUserPoints(sender) < cost) {
+                $.say($.whisperPrefix(sender) + ' you don\'t have a enough pasties. Wheelspin costs 10,000')
+                return;
+            }
+
+            if (users[sender] !== undefined) {
+                if(users[sender] + (userCoolDown * 1000) > $.systemTime()) {
+                    $.say($.whisperPrefix(sender) + ' wheelspin is still on cooldown for YOU.');
+                    return;
+                }
+            }
+
+            users[sender] = $.systemTime();
+            $.inidb.decr('points', sender, cost);
+            $.alertspollssocket.alertImage('wheelspin.gif, 6, 1.0,color: #BAB2B3;font-size: 50px;font-family: Cantarell;width: 600px;word-wrap: break-word;,' + (sender) + ' paid ' + cost + ' to spin the wheel');
+            $.say((sender) + ' paid ' + cost + ' to spin the wheel!');
+            var time = String($.getCurrentLocalTimeString('h:mm:ss a', 'America/Phoenix'));
+            $.writeToFile( time + ' ' + sender + ' Wheel Spin', './addons/wheelspins.txt', true);
+        }
+    });
+
+    $.bind('initReady', function() {
+        $.registerChatCommand('./custom/wheelSpin.js', 'wheelspin', 7)
+    });
+})();
