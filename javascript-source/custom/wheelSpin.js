7d6ba481c246b71412e786baa753640b6c73a0e7
Psychoboy <psycoboy@gmail.com>
2022-02-27 17:23:20 -0700

    Update wheelSpin.js

--- javascript-source/custom/wheelSpin.js
+++ javascript-source/custom/wheelSpin.js
@@ -1,6 +1,7 @@
 (function() {
     var cost = 10000,
         userCoolDown = 3600, //1 hour in seconds
+        coolDown = $.getSetIniDbNumber('wheelspinSettings', 'coolDown', 60),
         users = [];
 
     $.bind('command', function(event) {
@@ -30,10 +31,19 @@
             $.say((sender) + ' paid ' + cost + ' to spin the wheel!');
             var time = String($.getCurrentLocalTimeString('h:mm:ss a', 'America/Phoenix'));
             $.writeToFile( time + ' ' + sender + ' Wheel Spin', './addons/wheelspins.txt', true);
+            $.coolDown.set('wheelspin', false, coolDown, false);
+        }
+
+        if(command.equalsIgnoreCase('wheelspincooldown')) {
+            coolDown = parseInt(action);
+            $.inidb.set('wheelspinSettings', 'coolDown', coolDown);
+            $.say($.whisperPrefix(sender) + 'Updated cooldown to ' + coolDown + ' seconds');
         }
     });
 
     $.bind('initReady', function() {
-        $.registerChatCommand('./custom/wheelSpin.js', 'wheelspin', 7)
+        $.registerChatCommand('./custom/wheelSpin.js', 'wheelspin', 7);
+        $.registerChatCommand('./custom/wheelSpin.js', 'wheelspincooldown', 1);
+
     });
 })();
