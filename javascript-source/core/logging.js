daadc3e0362d6c30db2619fc6aba9600e30b689e
Psychoboy <psycoboy@gmail.com>
2022-05-13 13:51:40 -0700

    Forgot comma

--- javascript-source/core/logging.js
+++ javascript-source/core/logging.js
@@ -237,7 +237,7 @@
                     'discord',
                     'warning',
                     'whispers'
-                ]
+                ],
                 logDirIdx,
                 datefmt = new java.text.SimpleDateFormat('dd-MM-yyyy'),
                 date,
